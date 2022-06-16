362a2d0dbc09ecc54ec31f7c2bcd27ba5e434df7
Ronnakon <ronnakon_nisaensat@cmu.ac.th>
2022-06-16 17:13:55 +0700

    add to db

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function onLoad() {
         .then(incomeData => {
             defaultHide()
             console.log(incomeData.data)
-            addAnimeList(incomeData.data,0)
+            addAnimeList(incomeData.data, 0)
         }
         )
 }
@@ -15,7 +15,7 @@ function onLoad() {
 var defaultPage = document.getElementById('defaultPage')
 var interestedPage = document.getElementById('interestedPage')
 var detailPage = document.getElementById('detailPage')
-function defaultHide(){
+function defaultHide() {
     interestedPage.style.display = 'none'
     detailPage.style.display = 'none'
 }
@@ -25,12 +25,22 @@ function hideAll() {
     detailPage.style.display = 'none'
 }
 document.getElementById('homeBtn').addEventListener('click', function () {
-    onLoad()
-    
+    hideAll()
+    defaultPage.style.display = 'block'
+    fetch('https://api.jikan.moe/v4/seasons/now').then(response => {
+        return response.json()
+    })
+        .then(incomeData => {
+            defaultHide()
+            console.log(incomeData.data)
+            addAnimeList(incomeData.data, 0)
+        }
+        )
 })
 document.getElementById('interestedBtn').addEventListener('click', function () {
     hideAll()
     interestedPage.style.display = 'block'
+    showInterested()
 
 })
 // document.getElementById('detailPage').addEventListener('click', function () {
@@ -40,8 +50,8 @@ document.getElementById('interestedBtn').addEventListener('click', function () {
 // })
 
 
-function addAnimeList(aniList,status) {
-    
+function addAnimeList(aniList, status) {
+
     let divBody = document.getElementById('defaultPage')
     divBody.innerHTML = ""
     let text = document.createElement('h2')
@@ -54,10 +64,10 @@ function addAnimeList(aniList,status) {
     contentDiv.classList.add('flex-wrap')
     divBody.appendChild(contentDiv)
     for (ani of aniList) {
-        addStudentToDiv(status, ani)
+        addAnimeToDiv(status, ani)
     }
 }
-function addStudentToDiv(status,data){
+function addAnimeToDiv(status, data) {
     let divBody = document.getElementById('contentCard')
     let card = document.createElement('div')
     card.classList.add('card')
@@ -67,14 +77,14 @@ function addStudentToDiv(status,data){
     card.style.height = "25rem"
     card.style.minWidth = '15rem'
     card.classList.add('btn')
-    
+
     let image = document.createElement('img')
-    if (status == 0){
+    if (status == 0) {
         image.src = `${data.images.jpg.image_url} `
-    }else if (status == 1){
+    } else if (status == 1) {
         image.src = `${data.image_url} `
     }
-    
+
     image.classList.add('card-img-top')
     image.style.height = '22rem'
     let cardBody = document.createElement('div')
@@ -89,7 +99,10 @@ function addStudentToDiv(status,data){
     card.appendChild(image)
     card.appendChild(cardBody)
     card.addEventListener('dblclick', function () {
-        showAnimeDetail(data)
+        let text = `Add ${data.title} to favorites?`;
+        if (confirm(text)) {
+            addToFav(data, status)
+        }
     })
     divBody.appendChild(card)
 }
@@ -103,10 +116,70 @@ document.getElementById('searchBtn').addEventListener('click', function () {
     })
         .then(incomeData => {
             defaultHide()
-            
+
             console.log(incomeData.results)
-            addAnimeList(incomeData.results,1)
+            addAnimeList(incomeData.results, 1)
         }
         )
 })
 
+function addToFav(data, status) {
+    fetch('https://se104-project-backend.du.r.appspot.com/movies', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            id: '642110327',
+            movie: {
+                url: `${data.url}`,
+                image_url: `${data.images.jpg.image_url}`,
+                title: `${data.title}`,
+                synopsis: `${data.synopsis}`,
+                type: `${data.type}`,
+                episodes: `${data.episodes}`,
+                score: `${data.score}`,
+                rated: `${data.rating}`,
+            }
+        })
+    }).then(response => {
+        if (response.status === 200) {
+            return response.json()
+        } else {
+            throw Error(response.statusText)
+        }
+    }).then(data => {
+        console.log('success', data)
+        alert(`anime ${data.title} is now added`)
+        showInterested()
+    }).catch(error => {
+        return null
+    })
+}
+
+function showInterested() {
+    fetch('https://se104-project-backend.du.r.appspot.com/movies/642110327').then(response => {
+        return response.json()
+    })
+        .then(incomeData => {
+            console.log(incomeData)
+            showInterestedList(incomeData)
+        }
+    )
+}
+
+function showInterestedList(aniList) {
+
+    let divBody = document.getElementById('interestedPage')
+    divBody.innerHTML = ""
+    let text = document.createElement('h2')
+    text.innerHTML = 'My anime List'
+    divBody.appendChild(text)
+    let contentDiv = document.createElement('div')
+    contentDiv.id = 'contentCard'
+    contentDiv.classList.add('container')
+    contentDiv.classList.add('d-flex')
+    contentDiv.classList.add('flex-wrap')
+    divBody.appendChild(contentDiv)
+    
+}
