7c8ed634f33b717b706ccd3020dccaa47d9ffd1d
Ronnakon <ronnakon_nisaensat@cmu.ac.th>
2022-06-16 16:29:46 +0700

    searchAnime

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function onLoad() {
         .then(incomeData => {
             defaultHide()
             console.log(incomeData.data)
-            addAnimeList(incomeData.data)
+            addAnimeList(incomeData.data,0)
         }
         )
 }
@@ -25,8 +25,7 @@ function hideAll() {
     detailPage.style.display = 'none'
 }
 document.getElementById('homeBtn').addEventListener('click', function () {
-    hideAll()
-    defaultPage.style.display = 'block'
+    onLoad()
     
 })
 document.getElementById('interestedBtn').addEventListener('click', function () {
@@ -41,8 +40,8 @@ document.getElementById('interestedBtn').addEventListener('click', function () {
 // })
 
 
-function addAnimeList(aniList) {
-    let counter = 1;
+function addAnimeList(aniList,status) {
+    
     let divBody = document.getElementById('defaultPage')
     divBody.innerHTML = ""
     let text = document.createElement('h2')
@@ -55,20 +54,59 @@ function addAnimeList(aniList) {
     contentDiv.classList.add('flex-wrap')
     divBody.appendChild(contentDiv)
     for (ani of aniList) {
-        addStudentToDiv(counter++, ani)
+        addStudentToDiv(status, ani)
     }
 }
-function addStudentToDiv(index,data){
+function addStudentToDiv(status,data){
     let divBody = document.getElementById('contentCard')
     let card = document.createElement('div')
     card.classList.add('card')
-    card.classList.add('m-2')
-    card.style.width = "12rem";
-    card.style.height = "16rem"
-    let image = document.createElement('img')
-    image.src = `${data.images.jpg.image_url} `
-    image.classList.add('card-img-top')
-    card.appendChild(image)
+    card.classList.add('mx-2')
+    card.classList.add('my-5')
+    card.style.width = "15rem"
+    card.style.height = "25rem"
+    card.style.minWidth = '15rem'
+    card.classList.add('btn')
     
+    let image = document.createElement('img')
+    if (status == 0){
+        image.src = `${data.images.jpg.image_url} `
+    }else if (status == 1){
+        image.src = `${data.image_url} `
+    }
+    
+    image.classList.add('card-img-top')
+    image.style.height = '22rem'
+    let cardBody = document.createElement('div')
+    cardBody.classList.add('card-body')
+    let infoText = document.createElement('p')
+    infoText.classList.add('card-text')
+    infoText.classList.add('w-75')
+    infoText.classList.add('d-inline-block')
+    infoText.classList.add('text-truncate')
+    infoText.innerText = `${data.title} `
+    cardBody.appendChild(infoText)
+    card.appendChild(image)
+    card.appendChild(cardBody)
+    card.addEventListener('dblclick', function () {
+        showAnimeDetail(data)
+    })
     divBody.appendChild(card)
 }
+
+document.getElementById('searchBtn').addEventListener('click', function () {
+    let searchText = document.getElementById('searchInput').value
+    hideAll()
+    defaultPage.style.display = 'block'
+    fetch(`https://api.jikan.moe/v3/search/anime?q=${searchText}`).then(response => {
+        return response.json()
+    })
+        .then(incomeData => {
+            defaultHide()
+            
+            console.log(incomeData.results)
+            addAnimeList(incomeData.results,1)
+        }
+        )
+})
+
